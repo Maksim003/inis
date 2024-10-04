d50bae8a3df7783151274d620416c03643c62996
Maksim003 <savchik00@list.ru>
2024-10-04 10:02:19 +0300

    third commit

--- Laba2/details.js
+++ Laba2/details.js
@@ -6,40 +6,6 @@ if (shirtData) {
     document.getElementById('shirt-price').textContent = `Цена: ${shirtData.price || 'Нет цены'}`;
     document.getElementById('shirt-description').textContent = 'Beautiful T-Shirt';
 
-
-    /*const colorButtonsContainer = document.getElementById('color-buttons-container');
-    let selectedColor = Object.keys(shirtData.colors)[0];
-    let currentSide = 'front';
-
-    const frontButton = document.createElement('button');
-    frontButton.textContent = 'Front';
-    frontButton.onclick = function() {
-        currentSide = 'front';
-        document.getElementById('shirt-image').src = shirtData.colors[selectedColor].front || shirtData.default.front || '';
-    };
-    colorButtonsContainer.appendChild(frontButton);
-
-    const backButton = document.createElement('button');
-    backButton.textContent = 'Back';
-    backButton.onclick = function() {
-        currentSide = 'back';
-        document.getElementById('shirt-image').src = shirtData.colors[selectedColor].back || shirtData.default.back || '';
-    };
-    colorButtonsContainer.appendChild(backButton);
-
-    for (const color in shirtData.colors) {
-        const button = document.createElement('button');
-        button.textContent = color;
-        button.style.backgroundColor = color;
-        button.onclick = function() {
-            selectedColor = color;
-            if (currentSide === 'front') {
-                frontButton.onclick();
-            } else {
-                backButton.onclick();
-            }
-        };
-        colorButtonsContainer.appendChild(button);*/
     const colorButtonsContainer = document.getElementById('color-buttons');
     let selectedColor = Object.keys(shirtData.colors)[0];
     let currentSide = 'front';
@@ -56,7 +22,6 @@ if (shirtData) {
         document.getElementById('shirt-image').src = shirtData.colors[selectedColor].back || shirtData.default.back || '';
     };
 
-// Создание кнопок для цветов
     for (const color in shirtData.colors) {
         const button = document.createElement('button');
         button.textContent = color;
